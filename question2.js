8cc2219a22a2c073b60c9926027700eba3ec1d58
Egide RW <77799500+Egide250@users.noreply.github.com>
2022-05-03 04:27:22 -0700

    Add files via upload

--- question2.js
+++ question2.js
@@ -1,45 +1,48 @@
- const items = [ {name: 'Bike', price:100}, 
-  {name: 'TV', price:200}, 
-  {name: 'Album', price:10}, 
-  {name: 'Book', price:5}, 
-  {name: 'Phone', price:500}, 
-  {name: 'Computer', price:1000}
+/** @format */
+
+const item = [
+	{ name: "Bike", price: 100 },
+	{ name: "TV", price: 200 },
+	{ name: "Album", price: 10 },
+	{ name: "Book", price: 5 },
+	{ name: "Phone", price: 500 },
+	{ name: "Computer", price: 1000 },
 ];
 
-//Sub Question 1
-const cheapest = items.reduce(
-  (first, second) =>
-    first.price < second.price
-      ? first
-      : second
+// Question 1
+const cheapestItem = item.reduce((first, second) =>
+	first.price < second.price ? first : second
 );
-console.log("Q1. Without money, I will Buy:");
-console.log(cheapest.name+" with price of "+cheapest.price);
-
-//Sub Question 2
-const expensive = items.reduce(
-  (first, second) =>
-    first.price > second.price
-      ? first
-      : second
+console.log(
+	"Q1. Without money, I will Buy a " +
+		cheapestItem.name +
+		" with price of " +
+		cheapestItem.price
 );
-console.log("Q2. The expensive product is:");
-console.log(expensive.name+" with price of "+expensive.price);
 
-//Sub Question 3
-var tot = 0;
-items.forEach(total);
-function total(item) {
-    tot += item.price;
-} 
-document.write("Q3. The total price of all products is " + tot);
+// Question 2
+const expensiveItem = item.reduce((first, second) =>
+	first.price > second.price ? first : second
+);
+console.log(
+	"Q2. The expensive product is " +
+		expensiveItem.name +
+		" with price of " +
+		expensiveItem.price
+);
 
-//Sub Question 4
-var tot2 = 0;
-items.forEach(otherTotal);
-function otherTotal(item) {
-    if(item.price >= 10){
-        tot2 += item.price
-    }
-} 
-document.write("Q4. The total without whose price is < 10 is "+tot2);
+// Question 3
+let total = 0;
+item.forEach((values) => {
+	total += values.price;
+});
+console.log("Q3. The total price of all products is " + total);
+
+// Question 4
+let total2 = 0;
+item.forEach((values) => {
+	if (values.price >= 10) {
+		total2 += values.price;
+	}
+});
+console.log("Q4. The total without whose price is < 10 is " + total2);
